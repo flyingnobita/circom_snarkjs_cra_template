14ebaaa9654f03320a332591d7f800880621e81c
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2022-09-02 02:22:17 +0800

    Update gen.sh

--- packages/circuits/gen.sh
+++ packages/circuits/gen.sh
@@ -3,9 +3,8 @@
 set -ex
 
 ## Check for 2 input arguments
-if [ ! $# -eq 2 ]
-  then
-    echo "Usage: `basename $0` CIRCUIT PROVE[plonk|groth16]" 
+if [ ! $# -eq 2 ]; then
+    echo "Usage: $(basename $0) CIRCUIT PROVE[plonk|groth16]"
     exit 0
 fi
 
@@ -35,7 +34,7 @@ circom $1.circom --r1cs --sym --wasm --c
 ### 1.2. Export R1CS to JSON for readability
 snarkjs r1cs export json $1.r1cs $1.r1cs.json
 
-## 3. Compute Witness  
+## 3. Compute Witness
 node $1_js/generate_witness.js $1_js/$1.wasm $1_input.json $1_witness.wtns
 
 ### 4.2 Generate `zkey`
@@ -53,7 +52,12 @@ if [ ! -d proof ]; then
 fi
 snarkjs $2 prove zkeys/$1_$2_final.zkey $1_witness.wtns proof/$1_proof_$2.json proof/$1_public_$2.json
 
-## 7.2. Generate `verifyProof()` Call Parameters 
-snarkjs zkey export soliditycalldata proof/$1_public_$2.json proof/$1_proof_$2.json > $1_sol_calldata.txt
+## 7.2. Generate `verifyProof()` Call Parameters
+snarkjs zkey export soliditycalldata proof/$1_public_$2.json proof/$1_proof_$2.json >$1_sol_calldata_$2.txt
+
+## 7.3. Generate Verify Solidity Contract
+snarkjs zkey export solidityverifier zkeys/$1_$2_final.zkey proof/$1_$2.sol
+
+
 
 set +ex
